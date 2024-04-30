e36cda32bc5b95aeb13a9c914855b0055632d95e
abdullahal5 <abdullahalfahin183@gmail.com>
2024-04-30 05:54:52 -0700

    crud

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("boss is sitting");
+  res.send("Server is running");
 });
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.zelnjpd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -24,20 +29,70 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    console.log("Connected to MongoDB!");
+
     const employeesCollection = client.db("marcuricit").collection("employees");
 
-    
+    app.post("/employees", async (req, res) => {
+      const employee = req.body;
+      const result = await employeesCollection.insertOne(employee);
+      res.send(result);
+    });
+
+    app.get("/employees", async (req, res) => {
+      const result = await employeesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/employees/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await employeesCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/employees/:id", async (req, res) => {
+      const body = req.body;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateInfo = {
+        $set: {
+          firstName: body.firstName,
+          lastName: body.lastName,
+          email: body.email,
+          age: body.age,
+          salary: body.salary,
+          city: body.city,
+          position: body.position,
+          company: body.company,
+          number: body.number,
+        },
+      };
+      const result = await employeesCollection.updateOne(
+        query,
+        updateInfo,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await employeesCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   } finally {
-    await client.close();
   }
 }
 run().catch(console.dir);
-
-app.listen(port, () => {
-  console.log(`Bistro boss is sitting on port ${port}`);
-});
